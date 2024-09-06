a6f1a8304e047326ec9061a0af4d9c36975578f3
Vivi <lenardvivien@yahoo.de>
2024-09-06 19:17:05 +0200

    init

---
+++ hardhat.config.cjs
@@ -0,0 +1,13 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.24",
+// };
+
+
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.4",
+};
